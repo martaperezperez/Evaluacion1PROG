6dfd8ba7cf71defe000ebf297d3b7ad7d156c77e
Marta Pérez Pérez <mperezperez@danielcastelao.org>
2021-11-08 08:51:19 +0100

    Boletin7_3 8/11/21

--- src/Boletin7_3/Signo.java
+++ src/Boletin7_3/Signo.java
@@ -4,26 +4,25 @@ import java.util.Scanner;
 
 public class Signo {
     Scanner sc;
-    public void Signo(){
-        sc= new Scanner(System.in);
-        int num= leerEnteiro();
-        if (num>0){
+
+    public void Signo() {
+        sc = new Scanner(System.in);
+        int num = leerEnteiro();
+        if (num > 0) {
             System.out.println("o signo é +");
         }
-        else if (num==0){
+        else if (num == 0) {
             System.out.println("o numero é 0");
         }
-
-        else{
+        else {
             System.out.println("o signo é -");
         }
     }
 
 
-
-    public int leerEnteiro(){
+    public int leerEnteiro() {
         System.out.println("teclear numero: ");
-        int num=sc.nextInt();
+        int num = sc.nextInt();
         return num;
     }
 }
