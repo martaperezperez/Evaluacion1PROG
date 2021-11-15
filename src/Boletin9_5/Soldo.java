97d6da83dbac186c079e87d0a20e1167f612b680
Marta Pérez Pérez <mperezperez@danielcastelao.org>
2021-11-15 09:08:14 +0100

    Boletin9_3/4/5 15/11/21

--- src/Boletin9_5/Soldo.java
+++ src/Boletin9_5/Soldo.java
@@ -7,21 +7,23 @@ public class Soldo {
     private int numTrabajadores;
     private int contador1=0;
     private int contador2=0;
+    private int porcentaje;
     public Soldo(){
     }
     public void soldoDosTrabajadores(){
         numTrabajadores= Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de trabajadores"));
         while (i<numTrabajadores){
             int soldo=Integer.parseInt(JOptionPane.showInputDialog("Mete el sueldo de el trabajador" + (i+1)));
-            if (soldo>1000 && soldo<1750){
+            if (soldo>=1000 && soldo<=1750){
                 contador1++;
             }
             else if (soldo<100){
                 contador2++;
+                porcentaje=(100/numTrabajadores)*contador2;
             }
             i++;
         }
         float contador3=((contador2+100)/numTrabajadores);
-        System.out.println("Hay " + contador1 + " trabajadores que cobran entre 1000 y 1750 y hay " + contador3 + "Que no llegan a cobrar 1000€" );
+        System.out.println("Hay " + contador1 + " trabajadores que cobran entre 1000 y 1750 y hay " + contador2 + "Que no llegan a cobrar 1000€" );
     }
 }
