4c907e5fde61eb0696edefee5acc2145d48b7de2
Marta Pérez Pérez <mperezperez@danielcastelao.org>
2021-11-29 09:11:09 +0100

    Boletin14 29/11/2021

--- src/Boletin14/Boletin14.java
+++ src/Boletin14/Boletin14.java
@@ -1,4 +1,13 @@
 package Boletin14;
 
 public class Boletin14 {
+    public static void main(String[] args) {
+        Publicacion publi=new Publicacion();
+        publi.toString();
+        Libros li= new Libros();
+        li.toString();
+        Revistas re=new Revistas();
+        re.toString();
+    }
+
 }
