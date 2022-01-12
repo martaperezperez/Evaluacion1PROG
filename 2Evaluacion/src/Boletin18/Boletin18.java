c1d793dc452baeac906d6646e2ef0b325bcac73a
Marta Pérez Pérez <mperezperez@danielcastelao.org>
2022-01-12 10:50:32 +0100

    Boletin 18 12/01/2022

--- 2Evaluacion/src/Boletin18/Boletin18.java
+++ 2Evaluacion/src/Boletin18/Boletin18.java
@@ -1,4 +1,14 @@
 package Boletin18;
 
 public class Boletin18 {
+    public static void main(String[] args) throws TemperaturaErradaExcepcion {
+        ConversorTemperaturas ex= new ConversorTemperaturas();
+        try{
+            ex.centigradosAFharenheit();
+            ex.centígradosAReamur();
+        }catch(ArithmeticException exs){
+            System.out.println("error ");
+        }
+        System.out.println("fin do programa");
+    }
 }
