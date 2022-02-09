69a5c4aeaeea12e4811109b93913d5fad9b4113b
Marta Pérez Pérez <mperezperez@danielcastelao.org>
2022-02-09 12:34:38 +0100

    Boletin 23 09/02/2022

---
+++ 2Evaluacion/src/Boletin23/Boletin23.java
@@ -0,0 +1,20 @@
+package Boletin23;
+
+public class Boletin23 {
+    public static void main(String[] args) {
+        Ejercicios obx= new Ejercicios();
+        // obx.ejercicio1();
+        // obx.ejercicio2();
+        // obx.ejercicio3();
+        //  obx.ejercicio4();
+        //  obx.ejercicio5();
+        //  obx.ejercicio6();
+        //  obx.ejercicio7(args);
+        //  obx.ejercicio8(args);
+        //  obx.ejercicio9();
+        //obx.ejercicio10();
+        // obx.ejercicio11();
+
+
+    }
+}
